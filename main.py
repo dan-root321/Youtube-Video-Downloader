3f363022687ef7bd7a17c4a48c3297d986e2176f
#Kali <brucaldan@gmail.com>
2023-04-13 08:27:44 +0800

    Files upload

---
+++ main.py
@@ -0,0 +1,79 @@
+import tkinter
+import customtkinter
+from pytube import YouTube
+
+
+def download():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
+        video = ytObject.streams.get_highest_resolution()
+
+        title.configure(text=ytObject.title, text_color="white")
+        finishLabel.configure(text="")
+
+        video.download()
+        finishLabel.configure(text="Downloaded!")
+    except:
+        finishLabel.configure(text="Download Error", text_color="red")
+
+
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded  / total_size * 100
+    per = str(int(percentage_of_completion))
+    pPercentage.configure(text=per + '%')
+    pPercentage.update()
+
+    #update Progress bar
+    progressBar.set(float(percentage_of_completion) / 100 )
+
+
+# System settings
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+
+# App Frame
+app = customtkinter.CTk()
+
+app.geometry("720x480")
+app.title("Youtube Downloader")
+
+# Adding UI elements
+title = customtkinter.CTkLabel(app, text="Insert youtube link")
+title.pack(padx=10, pady=15)
+
+
+# Link input
+url_var = tkinter.StringVar()
+link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
+link.pack()
+
+# Finished Downloading
+finishLabel = customtkinter.CTkLabel(app, text = "")
+finishLabel.pack()
+
+# Progress Percentage
+pPercentage = customtkinter.CTkLabel(app, text="0%")
+pPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
+
+# Download Button
+dl = customtkinter.CTkButton(app, text="Download", command=download, height= 40, width=150)
+dl.pack(padx = 10, pady= 10)
+
+
+
+
+
+
+# Run app at loop
+app.mainloop()
+
+
